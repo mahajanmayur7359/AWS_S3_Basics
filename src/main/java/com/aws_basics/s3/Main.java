a088d7f5657805255d0986f40c4194fbd28e7d30
Mayur Mahajan <mahajanmayur7359@gmail.com>
2023-08-06 16:15:29 +0530

    Initial Commit

---
+++ src/main/java/com/aws_basics/s3/Main.java
@@ -0,0 +1,43 @@
+package com.aws_basics.s3;
+
+import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+
+public class Main {
+    public static void main(String[] args) {
+        //Add code here
+    }
+
+
+
+    //Method to create bucket
+    public static void createBucket(S3Client s3Client, String bucketName) {
+
+        try {
+            S3Waiter s3Waiter = s3Client.waiter();
+            CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            s3Client.createBucket(bucketRequest);
+
+            HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            // Wait until the bucket is created and print out the response.
+            WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
+            waiterResponse.matched().response().ifPresent(System.out::println);
+            System.out.println(bucketName + " is ready");
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+}

8b675d512eec9ddbf76207e26e8f2b9d4ebc753b
Mayur Mahajan <mahajanmayur7359@gmail.com>
2023-08-12 15:24:05 +0530

    Added logic to create and list S3 buckets

---
+++ src/main/java/com/aws_basics/s3/Constants.java
@@ -0,0 +1,11 @@
+package com.aws_basics.s3;
+
+import software.amazon.awssdk.regions.Region;
+
+public class Constants {
+
+    static final public Region awsRegion = Region.US_EAST_1;
+
+    static final public String buketNamePrefix = "aws-s3-basics-";
+
+}
--- src/main/java/com/aws_basics/s3/Main.java
+++ src/main/java/com/aws_basics/s3/Main.java
@@ -1,29 +1,53 @@
 package com.aws_basics.s3;
 
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+
 
 public class Main {
     public static void main(String[] args) {
-        //Add code here
+
+        S3Client s3Client = getS3Client(Constants.awsRegion);
+
+        boolean bucketCreationStatus = createBucket(s3Client, createBucketName());
+
+        if(bucketCreationStatus) {
+            listBuckets(s3Client);
+        }
+    }
+
+    //Create Unique Bucket name
+    private static String createBucketName(){
+        DateFormat dateObjectFormat = new SimpleDateFormat("ddMMyyyy-HHmmss");
+        return Constants.buketNamePrefix + dateObjectFormat.format(Calendar.getInstance().getTime());
+    }
+
+    //Create S3Client with required permission
+    private static S3Client getS3Client(Region region){
+        return S3Client.builder().region(region).build();
     }
 
 
-
     //Method to create bucket
-    public static void createBucket(S3Client s3Client, String bucketName) {
+    public static boolean createBucket(S3Client s3Client, String bucketName) {
 
         try {
-            S3Waiter s3Waiter = s3Client.waiter();
+            System.out.println("Creating S3 bucket....");
             CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
                     .bucket(bucketName)
                     .build();
-
             s3Client.createBucket(bucketRequest);
 
             HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
@@ -31,13 +55,25 @@ public class Main {
                     .build();
 
             // Wait until the bucket is created and print out the response.
+            S3Waiter s3Waiter = s3Client.waiter();
             WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
             waiterResponse.matched().response().ifPresent(System.out::println);
-            System.out.println(bucketName + " is ready");
+            System.out.println(bucketName + " is ready to use.");
+
+            return true;
 
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
+            return false;
+        }
+    }
+
+    //Method to list of buckets in S3
+    private static void listBuckets(S3Client s3Client){
+        List<Bucket> bucketLists = s3Client.listBuckets().buckets();
+        System.out.println("Listing all S3 buckets.....");
+        for (Bucket bucket : bucketLists){
+            System.out.println(bucket.name());
         }
     }
 }
